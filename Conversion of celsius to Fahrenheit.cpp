0ce688c5671c195af2cd9988caebef617b0f4fc1
Wambo04 <114184810+Wambo04@users.noreply.github.com>
2022-11-17 10:14:44 +0300

    Create Conversion of celsius to Fahrenheit.cpp

---
+++ Conversion of celsius to Fahrenheit.cpp
@@ -0,0 +1,12 @@
+//program to convert degrees Celsius to Fahrenheit 
+#include<iostream>
+using namespace std;
+int main()
+{
+float Celsius, Fahrenheit;
+cout<<"enter temperature in celcius"<<endl;
+cin>>Celsius;
+Fahrenheit =(celsius+32)*9/5;
+cout<<"temperature in Fahrenheit is"<<Fahrenheit <<endl;
+return 0;
+}
